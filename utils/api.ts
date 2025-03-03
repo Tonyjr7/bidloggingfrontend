56dfac24aef2f66f91367b945428f65387857ca7
Python_Pioneer <89539411+Tonyjr7@users.noreply.github.com>
2025-03-03 14:16:14 +0000

    Update api.ts

--- utils/api.ts
+++ utils/api.ts
@@ -1,5 +1,5 @@
 export function getApiUrl(path: string): string {
-    const baseUrl = process.env.NEXT_PUBLIC_API_BASE_URL || "https://todoapi-9e6k.onrender.com"
+    const baseUrl = process.env.NEXT_PUBLIC_API_BASE_URL || "https://bidloggingapi.onrender.com"
     return `${baseUrl}${path}`
   }
   
